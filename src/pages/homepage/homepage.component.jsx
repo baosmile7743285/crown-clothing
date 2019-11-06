03974cbbf9246fb09b8b0d34cef2f67b5de28342
Bảo Nguyễn <baosmile7743285@gmail.com>
2019-11-06 11:44:52 +0700

    update router and routing in our project

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Directory from '../../Components/directory/directory.component';
+
+import './/hompage.styles.scss'
+const HomePage = () => (
+    <Directory/>
+)
+
+export default HomePage;
