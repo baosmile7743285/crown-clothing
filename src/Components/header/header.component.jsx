e099e5b7dedf6a02feb27fdbc59038ec5099d8bd
Bảo Nguyễn <baosmile7743285@gmail.com>
2019-11-12 09:48:19 +0700

    add redux reducer and action to header and app component

--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './header.styles.scss';
+
+import {connect} from 'react-redux';
+
 import {Link} from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
@@ -24,5 +27,8 @@ const Header = ({currentUser}) => (
         </div>
     </div>
 )
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
 
-export default Header;
+export default connect(mapStateToProps)(Header);
